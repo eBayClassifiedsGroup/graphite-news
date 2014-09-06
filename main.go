16d7f15d6af7ecb0c543d245392df45453e895d7
Jilles Oldenbeuving <ojilles@gmail.com>
2014-09-07 00:08:01 +0200

    got some global state going

--- main.go
+++ main.go
@@ -7,33 +7,50 @@ import (
 	"net/http"
 	"regexp"
 	"strings"
+	"sync"
 )
 
-func makeHandler(fn func(http.ResponseWriter, *http.Request, []string)) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		fn(w, r)
-	}
+type state struct {
+	*sync.RWMutex                   // inherits locking methods
+	Vals          map[string]string // map ids to values
 }
 
-func viewHandler(w http.ResponseWriter, r *http.Request, dss []string) {
+// declare a globally scoped State variable, otherwise
+// the request handlers can't get to it. If there is a better
+// way to do this, plmk.
+var State = &state{&sync.RWMutex{}, map[string]string{}}
+
+func viewHandler(w http.ResponseWriter, r *http.Request) {
 	var log = logging.MustGetLogger("example")
+	log.Notice("viewHandler: acquiring lock")
+
+	State.RLock()         // grab a lock, but then don't forget to
+	defer State.RUnlock() // unlock it again once we're done
+
 	log.Info(fmt.Sprintf("Request for %s\n", r.URL.Path))
-	fmt.Fprintf(w, "Hi there, I love %s %s!", r.URL.Path[1:], dss)
+	fmt.Fprintf(w, "Hi there, I love %s %s!", r.URL.Path[1:], State.Vals)
+}
+
+func addItemToState(line string) {
+	var log = logging.MustGetLogger("example")
+	log.Notice("addItemToState: acquiring lock")
+	State.Lock()
+	defer State.Unlock()
+	State.Vals["bogus"] = line
 }
 
 func tailLogfile(dss []string, c chan string) {
 	var log = logging.MustGetLogger("example")
 
-	//var dss []string
 	var dataPath = regexp.MustCompile(`\[creates\] creating (database) file .*/whisper/(.*)\.wsp`)
 	t, err := tail.TailFile("./creates.log", tail.Config{Follow: true, ReOpen: true, MustExist: true})
 	if err == nil {
 		for line := range t.Lines {
 			match := dataPath.FindStringSubmatch(line.Text)
-			//	log.Info(match)
 			if len(match) > 0 {
 				log.Info(line.Text)
 				ds := fmt.Sprintf("%v", strings.Replace(match[2], `/`, `.`, -1))
+				addItemToState(ds)
 				dss = append(dss, ds)
 				log.Notice(fmt.Sprintf("Found new datasource, total: %v, newly added: %s", len(dss), ds))
 			}
@@ -52,13 +69,15 @@ func main() {
 	logging.SetFormatter(logging.MustStringFormatter(format))
 	log.Info("Graphite News -- Showing which new metrics are available since 2014\n")
 	log.Notice("Graphite News -- Serving UI on: http://localhost:2934\n")
-#http://stackoverflow.com/questions/18487923/golang-storing-caching-values-to-be-served-in-following-http-requests
-	http.HandleFunc("/view/", makeHandler(viewHandler))
+
+	// http://stackoverflow.com/questions/18487923/golang-storing-caching-values-to-be-served-in-following-http-requests
+
+	http.HandleFunc("/view/", viewHandler)
 	go http.ListenAndServe(":2934", nil)
 	go tailLogfile(dss, error_channel)
 
 	log.Notice("Graphite News -- Showing which new metrics are available since 2014\n")
 
 	// Wait for errors to appear then shut down
-	log.Notice(<-error_channel)
+	log.Error(<-error_channel)
 }
