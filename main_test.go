704cd45b3a20b12b07599cf51f7c96c1f20eca0c
Jilles Oldenbeuving <ojilles@gmail.com>
2014-09-14 21:00:06 +0200

    fail tests if Create_date is the nil-date (eg not parsed properly)

--- main_test.go
+++ main_test.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"net/http"
+	"net/http/httptest"
 	"testing"
 )
 
@@ -13,6 +15,29 @@ func BenchmarkHello(b *testing.B) {
 }
 
 // All state is not reset
+
+func TestGettingAsset(t *testing.T) {
+	data, _ := Asset("index.html")
+	if len(data) == 0 {
+		t.Fatal("Could not load static asset index.html")
+	}
+}
+
+func TestHTTPServingAssets(t *testing.T) {
+	handler := func(w http.ResponseWriter, r *http.Request) {
+		http.Error(w, "something failed", http.StatusInternalServerError)
+	}
+
+	req, err := http.NewRequest("GET", "http://www.google.com", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	w := httptest.NewRecorder()
+	handler(w, req)
+
+	t.Fatal(fmt.Sprintf("%+v", w))
+}
 
 func TestSingleDsIntoState(t *testing.T) {
 	tmp := len(State.Vals)
@@ -45,7 +70,7 @@ func TestDontStoreMoreThan1k(t *testing.T) {
 	const testString string = "TestDontStoreMoreThanMaxState, item: "
 
 	ds := Datasource{Name: "tmp"}
-	for i := 0; i < maxState+10; i++ {
+	for i := 0; i < maxState+11; i++ {
 		ds.Name = fmt.Sprintf("%v %v", testString, i)
 		addItemToState(ds)
 	}
@@ -70,13 +95,13 @@ func TestParsing(t *testing.T) {
 
 	// integer indicates with how much the count of internal state should go up
 	// (could be 0, eg. not valid test input, or ds already captured)
+	// Also, each line should have a non-nil Create_date
 	var testCases = []testpair{
 		{1, "launchctl-carbon.stdout:24/08/2014 17:59:53 :: [creates] creating database file /opt/graphite/storage/whisper/mac-mini_local/collectd/df-Volumes-Recovery_HD/df_complex-free.wsp (archive=[(60, 525600), (600, 518400)] xff=None agg=None)"},
 		{0, "astt"},
 		{0, "launchctl-carbon.stdout:24/08/2014 17:59:53 :: [creates] creating database file / (archive=[(60, 525600), (600, 518400)] xff=None agg=None)"},
 		{1, "13/09/2014 23:10:56 :: [creates] creating database file /opt/graphite/storage/whisper/local/random/diceroll.wsp (archive=[(60, 525600), (600, 518400)] xff=None agg=None)"},
 	}
-
 	State.Vals = nil // start fresh
 	prev_count := len(State.Vals)
 
@@ -87,5 +112,12 @@ func TestParsing(t *testing.T) {
 			t.Fatal(fmt.Sprintf("Parsed line, should have seen %v new entries, saw %v. Line: %v", test.incr, len(State.Vals)-prev_count, test.line))
 		}
 		prev_count = len(State.Vals)
+
+		// Check date parseing
+		last_ds := State.Vals[len(State.Vals)-1:][0]
+		// Do any checking on the actual values for the data source (not complete yet)
+		if last_ds.Create_date.IsZero() {
+			t.Fatal(fmt.Sprintf("Data source has invalid Create_date: %+v", last_ds))
+		}
 	}
 }
