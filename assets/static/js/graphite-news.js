9a8fdaa3dbd2063a3635934781ad8bc9baa128a7
Jilles Oldenbeuving <ojilles@gmail.com>
2014-10-02 00:05:59 +0200

    indented javascript properly

--- assets/static/js/graphite-news.js
+++ assets/static/js/graphite-news.js
@@ -3,35 +3,35 @@ var gn = {};
 gn.JsonPullInterval = 0;
 gn.GraphiteURL = '';
 gn.getConfig = function() {
-  var jqxhr = $.getJSON( "/config/", function() {
-  })
-  .done(function() {
-    data = jqxhr.responseJSON;
-    gn.JsonPullInterval = data.JsonPullInterval;
-    gn.GraphiteURL = data.GraphiteURL;
-    gn.AllowDsDeletes = data.AllowDsDeletes;
+	var jqxhr = $.getJSON( "/config/", function() {
+	})
+	.done(function() {
+		data = jqxhr.responseJSON;
+		gn.JsonPullInterval = data.JsonPullInterval;
+		gn.GraphiteURL = data.GraphiteURL;
+		gn.AllowDsDeletes = data.AllowDsDeletes;
 
-    // New config is read in, but the interval is attached to the
-    // timer and wont be updated dynamically. So depending on the 
-    // current state (active/inactive) we need to cycle that
-    if (gn.timer === undefined) {
-      // not running, noop
-    } else {
-      gn.toggle();
-      gn.toggle();
-    }
-  });
+		// New config is read in, but the interval is attached to the
+		// timer and wont be updated dynamically. So depending on the 
+		// current state (active/inactive) we need to cycle that
+		if (gn.timer === undefined) {
+			// not running, noop
+		} else {
+			gn.toggle();
+			gn.toggle();
+		}
+	});
 }
 
 function template(row, dss) {
-  row.find('.item_name').text(dss.Name);
-  row.find('.item_date').html("<abbr class='timeago' title='"+dss.Create_date+"'>"+dss.Create_date+"</abbr>");
-  row.find('.item_options').text(dss.Params);
-  return row;
+	row.find('.item_name').text(dss.Name);
+	row.find('.item_date').html("<abbr class='timeago' title='"+dss.Create_date+"'>"+dss.Create_date+"</abbr>");
+	row.find('.item_options').text(dss.Params);
+	return row;
 }
 
 function endsWith(str, suffix) {
-	    return str.indexOf(suffix, str.length - suffix.length) !== -1;
+	return str.indexOf(suffix, str.length - suffix.length) !== -1;
 }
 
 gn.RemoveEnabledHTML = function() {
@@ -55,7 +55,7 @@ gn.GraphiteImg = function(dsname, dsdate, width, derivative, editlink) {
 	} else {
 		derivative = false
 	}
-	
+
 	if ((width < 50) || (width === undefined)) { width = 800 /* some default */ }
 	if (editlink === undefined) { editlink = false }
 	if (!editlink) { render = "render/" } else { render = "" }
@@ -76,160 +76,160 @@ gn.GraphiteImg = function(dsname, dsdate, width, derivative, editlink) {
 // Update the set of data sources in the table, filter out
 // the ones that we already have based on DS name.
 gn.updateDs = function() {
-  var jqxhr = $.getJSON( "/json/", function() {
-    // initial success on calling getJSON
-  })
-  .done(function() {
-    gn.serverActive();
-     data = jqxhr.responseJSON;
-    $("#dscount").text(data.length);
-    $.map(data, function(el) {
-      // map all the data onto the HTML table
-      // if el.Name already exists, just skip it
-      if ($('span').filter(
-        function (index) { return $(this).text() == el.Name; }
-      ).length == 0)
-        {
-          var newRow = $('#cart .template').clone().removeClass('template');
-          template(newRow, el)
-          .prependTo('#cart')
-          .fadeIn()
+	var jqxhr = $.getJSON( "/json/", function() {
+		// initial success on calling getJSON
+	})
+	.done(function() {
+		gn.serverActive();
+		data = jqxhr.responseJSON;
+		$("#dscount").text(data.length);
+		$.map(data, function(el) {
+			// map all the data onto the HTML table
+			// if el.Name already exists, just skip it
+			if ($('span').filter(
+				function (index) { return $(this).text() == el.Name; }
+			).length == 0)
+		{
+			var newRow = $('#cart .template').clone().removeClass('template');
+			template(newRow, el)
+			.prependTo('#cart')
+			.fadeIn()
 
-          // Add hover/click event listners to the table, because
-          // I don't know jQuery well enough to solve this in a different
-          // way (prependTo call looses those listners)
-          .hover(function() {
-            $(this).addClass('gnhover');
-          }, function() {
-            $(this).removeClass('gnhover');
-          })
-	  // on click, add a row underneath with the graph(s)
-          .click(function() {
-            // If we have a .timeseries row underneath us, all we need
-            // to do is remove them and we're done (allows for click, click once more
-            // behaviour on normal ds td's)
-		// TODO, don't know how to do yet!
+			// Add hover/click event listners to the table, because
+			// I don't know jQuery well enough to solve this in a different
+			// way (prependTo call looses those listners)
+			.hover(function() {
+				$(this).addClass('gnhover');
+			}, function() {
+				$(this).removeClass('gnhover');
+			})
+			// on click, add a row underneath with the graph(s)
+			.click(function() {
+				// If we have a .timeseries row underneath us, all we need
+				// to do is remove them and we're done (allows for click, click once more
+				// behaviour on normal ds td's)
+				// TODO, don't know how to do yet!
 
-            // find any and all tr's with class timeseries and remove them (want to keep UI
-            // simple for now and only have one open at a time
-            $('#cart .timeseries').remove();
+				// find any and all tr's with class timeseries and remove them (want to keep UI
+				// simple for now and only have one open at a time
+				$('#cart .timeseries').remove();
 
-            // now copy a template and add that behind the row that was just clicked
-            var tmp = $('#cart .templateds').clone()
-              .removeClass('templateds')
-              .addClass('timeseries');
-            tmp.find('td:first')
-              .html(
-		"<div class='timeseriescontainer'>"
-		+"  <img class='img-rounded' src=\""+
-                gn.GraphiteImg(
-                  $(this).find("td:first").text(),
-                  "none",
-                  Math.floor($(this).width() * 0.9))+"\">"
-		+ '<span class="tsbtntoolbar">'
-		+ '  <div class="btn-group btn-group-sm">'
-		+ '    <a href="'+gn.GraphiteImg($(this).find("td:first").text(),"none",undefined,undefined,true)+'" type="button" class="btn btn-default">Edit</button>'
-		+ '    <a id="btnRemove" href="" type="button" class="btn btn-default'+ gn.RemoveEnabledHTML() +'">Remove</button>'
-		+ '  <div>'
-		+ '</span>'
-		+ '</div>'
-		)
-              .end()
-              .click( function(){ $(this).remove() })
-              .insertAfter('#cart .gnhover').fadeIn();
+				// now copy a template and add that behind the row that was just clicked
+				var tmp = $('#cart .templateds').clone()
+				.removeClass('templateds')
+				.addClass('timeseries');
+				tmp.find('td:first')
+				.html(
+					"<div class='timeseriescontainer'>"
+					+"  <img class='img-rounded' src=\""+
+						gn.GraphiteImg(
+							$(this).find("td:first").text(),
+					"none",
+					Math.floor($(this).width() * 0.9))+"\">"
+					+ '<span class="tsbtntoolbar">'
+					+ '  <div class="btn-group btn-group-sm">'
+					+ '    <a href="'+gn.GraphiteImg($(this).find("td:first").text(),"none",undefined,undefined,true)+'" type="button" class="btn btn-default">Edit</button>'
+					+ '    <a id="btnRemove" href="" type="button" class="btn btn-default'+ gn.RemoveEnabledHTML() +'">Remove</button>'
+					+ '  <div>'
+					+ '</span>'
+					+ '</div>'
+				)
+				.end()
+				.click( function(){ $(this).remove() })
+				.insertAfter('#cart .gnhover').fadeIn();
 
-	    // if we allow deletes, attach a handler
-            if (gn.AllowDsDeletes) {
-	      dsName = $(this).find("td:first").text();
-              $("#btnRemove").click(function() {
+				// if we allow deletes, attach a handler
+				if (gn.AllowDsDeletes) {
+					dsName = $(this).find("td:first").text();
+					$("#btnRemove").click(function() {
 
-              $.post("/delete/", { datasourcename: dsName } )
-                .done(function(data) {
-              	  $.growl({
-              	    title: '<strong>DELETING DATA SOURCE</strong><br/><i>'+dsName+'</i><br/>',
-              	    message: 'Deleting the data source worked. If it re-appears it<br/>is because the data source provider kept on sending metrics.'
-                  },{
-              	    type: 'success', delay: 5000, mouse_over: 'pauze', offset: 75
-                  });
-		  // remove the row from the table
-	          $('tr:has(td:contains("'+dsName+'"))').remove()
-                })
-                .fail(function(data) {
-              	  $.growl({
-              	    title: '<strong>DELETING DATA SOURCE</strong><br/><i>'+dsName+'</i><br/>',
-              	    message: 'Deleting the data source did not work<br/>Possible reasons</br><ul><li>Perhaps the item does not exist</li><li>Permissions problem server-side</li></ul>'
-                  },{
-              	    type: 'danger', delay: 5000, mouse_over: 'pauze', offset: 75
-                  });
-                })
-              ;
+						$.post("/delete/", { datasourcename: dsName } )
+						.done(function(data) {
+							$.growl({
+								title: '<strong>DELETING DATA SOURCE</strong><br/><i>'+dsName+'</i><br/>',
+								message: 'Deleting the data source worked. If it re-appears it<br/>is because the data source provider kept on sending metrics.'
+							},{
+								type: 'success', delay: 5000, mouse_over: 'pauze', offset: 75
+							});
+							// remove the row from the table
+							$('tr:has(td:contains("'+dsName+'"))').remove()
+						})
+						.fail(function(data) {
+							$.growl({
+								title: '<strong>DELETING DATA SOURCE</strong><br/><i>'+dsName+'</i><br/>',
+								message: 'Deleting the data source did not work<br/>Possible reasons</br><ul><li>Perhaps the item does not exist</li><li>Permissions problem server-side</li></ul>'
+							},{
+								type: 'danger', delay: 5000, mouse_over: 'pauze', offset: 75
+							});
+						})
+						;
 
-                // remove all opened timeseries
-                $('#cart .timeseries').remove()
-                return false
-              });
-            }
-	  });
-	  // update "Date" column with humanized timestamps
-	  jQuery("abbr.timeago").timeago();
-        }
-     });
-  })
-  .fail(function() {
-    gn.serverInactive();
-    console.log( "ERROR: Could not perform getJSON request. Server down?" );
-  })
+						// remove all opened timeseries
+						$('#cart .timeseries').remove()
+						return false
+					});
+				}
+			});
+			// update "Date" column with humanized timestamps
+			jQuery("abbr.timeago").timeago();
+		}
+		});
+	})
+	.fail(function() {
+		gn.serverInactive();
+		console.log( "ERROR: Could not perform getJSON request. Server down?" );
+	})
 }
 
 // functions to signal the status of connectivity to backend server
 gn.serverActive = function() {
-  $("#servercon").addClass('label-success');
-  $("#servercon").removeClass('label-danger');
-  $("#cart").removeClass('transparent');
+	$("#servercon").addClass('label-success');
+	$("#servercon").removeClass('label-danger');
+	$("#cart").removeClass('transparent');
 }
 gn.serverInactive = function() {
-  $("#servercon").removeClass('label-success');
-  $("#servercon").addClass('label-danger');
-  $("#cart").addClass('transparent');
+	$("#servercon").removeClass('label-success');
+	$("#servercon").addClass('label-danger');
+	$("#cart").addClass('transparent');
 }
 
 // Starting, Stopping and toggling our requests to the
 // server.
 gn.start = function() {
-  if(!gn.timer) {
-  gn.timer = setInterval(function() {gn.updateDs();}, gn.JsonPullInterval);
-  $("#hideButton").text('Pause')
-  $("#hideButton").toggleClass('btn-danger');
-  $("#hideButton").toggleClass('btn-success');
-  }
+	if(!gn.timer) {
+		gn.timer = setInterval(function() {gn.updateDs();}, gn.JsonPullInterval);
+		$("#hideButton").text('Pause')
+		$("#hideButton").toggleClass('btn-danger');
+		$("#hideButton").toggleClass('btn-success');
+	}
 }
 gn.stop = function() {
-  clearInterval(gn.timer);
-  gn.timer = undefined;
-  $("#hideButton").text('Activate')
-  $("#hideButton").toggleClass('btn-danger');
-  $("#hideButton").toggleClass('btn-success');
+	clearInterval(gn.timer);
+	gn.timer = undefined;
+	$("#hideButton").text('Activate')
+	$("#hideButton").toggleClass('btn-danger');
+	$("#hideButton").toggleClass('btn-success');
 }
 gn.toggle = function() {
-  if (gn.timer === undefined) {
-    gn.start();
-  } else {
-    gn.stop();
-  }
+	if (gn.timer === undefined) {
+		gn.start();
+	} else {
+		gn.stop();
+	}
 }
 
 var init = false;
 $(document).ready(function() {
-  if (!init){
-    init = true;
+	if (!init){
+		init = true;
 
-    gn.getConfig()
-    gn.start()
+		gn.getConfig()
+		gn.start()
 
-    $("#hideButton").click( function() {
-      gn.toggle();
-    });
-    gn.updateDs()
-    setInterval(function() {gn.getConfig();}, /* 1 minute */ 1*60*1000);
-  }
+		$("#hideButton").click( function() {
+			gn.toggle();
+		});
+		gn.updateDs()
+		setInterval(function() {gn.getConfig();}, /* 1 minute */ 1*60*1000);
+	}
 });
